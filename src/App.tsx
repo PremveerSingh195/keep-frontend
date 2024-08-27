77430521b871d1f7b0318b634796db59e36b0070
callsmasterofficial <partnerchc@outlook.com>
2024-08-27 10:23:03 +0530

    fd

--- src/App.tsx
+++ src/App.tsx
@@ -10,30 +10,9 @@ interface joke {
 
 function App() {
 
-  const [jokes, setJokes] = useState<joke[]>([])
-
-  useEffect(() => {
-    axios.get('http://localhost:3001/jokes')
-      .then((response) => {
-        setJokes(response.data)
-      }).catch((error)=> {
-        console.log(error);
-        
-      })
-  }, [])
-
-
   return (
     <div >
-      <h1 className='text-xl text-red-600'>hello world !</h1>
-      <div className=''>
-        {jokes.map((joke)=> (
-          <div key={joke.id}>
-            <h3>{joke.joke}</h3>
-            <p>{joke.description}</p>
-          </div>
-        ))}
-      </div>
+      <h1>Hello world</h1>
     </div>
   )
 }
