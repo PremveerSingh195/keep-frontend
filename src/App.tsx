235ddf6af95b428eb48105b16114c3412567b7c2
callsmasterofficial <partnerchc@outlook.com>
2024-09-04 16:43:43 +0530

    dshv

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import InputBox from "./components/InputBox"
-
 
 
 function App() {
+
+
   return (
-    <div className='w-screen h-screen flex justify-center items-center bg-black' >
-       <div className='h-96 w-96 rounded-md bg-gray-700 gap-4 flex justify-center flex-col items-center'>
-           <h1 className="text-2xl font-bold text-white">Login</h1>
-           <InputBox Inputtype="text" placeholder="Username"/>
-           <InputBox Inputtype="text" placeholder="Full Name"/>
-           <InputBox Inputtype="text" placeholder="Email"/>
-           <InputBox Inputtype="password" placeholder="Password"/>
-           <button className="text-xl font-semibold">Signup</button>
-       </div>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
+
 export default App
